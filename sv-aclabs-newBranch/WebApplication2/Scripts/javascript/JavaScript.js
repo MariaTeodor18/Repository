b3bf26db493f23ef9223140530bafa5f64ef396f
MariaTeodor18 <teodorvalentina@yahoo.com>
2017-05-16 21:03:23 +0300

    JS lab part 2

--- sv-aclabs-newBranch/WebApplication2/Scripts/javascript/JavaScript.js
+++ sv-aclabs-newBranch/WebApplication2/Scripts/javascript/JavaScript.js
@@ -10,6 +10,7 @@ $(document).ready(function() {
     };
 
     var updateResource = function (resourceName) {
+        console.log('apel ok');
 
         var start = new Date();
         var currentProduction = 0;
@@ -24,11 +25,30 @@ $(document).ready(function() {
 
         });
 
+        console.log(currentValue, currentProduction);
         var nextValue = (currentValue + ((start.getTime() - lastUpdate) / 1000 / 60 / 60) * currentProduction).toFixed(4);
 
         $(".res-value." + resourceName).text(nextValue); /*seteaza valoarea*/
         $(".res-lastUpdate." + resourceName).text(start.strftime("%Y-%m-%d %H:%M:%S"));
+
+        console.log(nextValue);
     };
 
     setInterval(updateResources, 1000);
+
+
+    var getMineDetailsHTML = function (mineId) {
+        $('#mine-details-container > .content').empty();
+        $('#mine-details-container > .content').load('/Mines/Details?mineId=' + mineId);
+        $('#mine-details-container').addClass('show');
+    }
+
+    $('.mine-details-btn').click(function () {
+        var mineId = $(this).data('mine-id');
+        getMineDetailsHTML(mineId);
+    });
+
+    $('#mine-details-container > .close-btn').click(function () {
+        $('#mine-details-container').removeClass('show');
+    });
 });
